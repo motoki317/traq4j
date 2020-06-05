b3e5345a48414f24410e696de8b40326f2186a01
motoki317 <motoki317@gmail.com>
2020-06-05 23:52:17 +0900

    fix gen

--- generate.sh
+++ generate.sh
@@ -4,16 +4,17 @@ set -eux
 
 # clean
 if [ -d ./src ]; then
-  FILES=$(find ./src -type f)
-  echo $FILES | xargs rm
+  rm -rf ./src
 fi
 
 # build
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
     -i https://raw.githubusercontent.com/traPtitech/traQ/master/docs/v3-api.yaml \
     -g java \
+    -c /local/config.yaml \
     -o /local/out
 
 mv ./out/src ./src
+rm ./src/main/AndroidManifest.xml
 
 rm -rf ./out
